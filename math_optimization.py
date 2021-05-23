27bac5131a5a9364bab5d911b173c3c912296482
Aildson Ferreira <aildson.ferreira@salvus.me>
2021-05-23 19:47:34 -0300

    implemented major functions

--- math_optimization.py
+++ math_optimization.py
@@ -2,67 +2,104 @@ from random import choices, randint, randrange, random
 from typing import List, Tuple
 
 
-# genetic representation of a solution, binary number, e.g. [1, 1, 1] = 7
-Genome = List[int]
-Population = List[Genome]
+# chromosome: genetic representation of a solution
+# binary number, e.g. [1, 1, 1] = 7
+Chromosome = List[int]
+Population = List[Chromosome]
 
 
 # generates one solution, given the length
-def generate_genome(length: int) -> Genome:
+def generate_chromosome(length: int) -> Chromosome:
     return choices([0, 1], k=length)
 
 
 # given the size of the population, generates one
-def generate_population(size: int, genome_length: int) -> Population:
-    return [generate_genome(genome_length) for _ in range(size)]
+def generate_population(size: int, chromosome_length: int) -> Population:
+    return [generate_chromosome(chromosome_length) for _ in range(size)]
 
 
-def fitness(genome: Genome, num: int) -> Tuple[Genome, int]:
-    genome_number = convert_binary(genome)
+# calculates the distance between the number (chromosome_number) and
+# the expected number (perfect_number), and returns it as a score
+# (fitness_score, e.g. chromosome_number = 7, perfect_number = 7 ->
+#   distance = 0, fitness_score = 100 -> 100%)
+def fitness(chromosome: Chromosome, num: int) -> int:
+    chromosome_number = convert_binary(chromosome)
     perfect_number = num
-    distance = abs(genome_number - perfect_number)
-    fitness_score = int(100 - (distance * 100)/(perfect_number))
-    return genome, fitness_score
+    distance = (perfect_number - chromosome_number)
+    fitness_score = int(100 - (distance * 100) / (perfect_number))
+    return fitness_score
 
 
-# def natural_selection(population: Population, )
+# selection occurs as a roulette based on the fitness score
+def natural_selection(population: Population, num: int):
+    return choices(
+        population,
+        weights=[fitness(chromosome, num)
+                 for chromosome in population],
+        k=2
+    )
 
 
-def crossover(gene_a: Genome, gene_b: Genome) -> Tuple[Genome, Genome]:
+def crossover(gene_a: Chromosome, gene_b: Chromosome) -> Tuple[Chromosome, Chromosome]:
     if len(gene_a) != len(gene_b):
-        raise ValueError("Genomes must have the same length")
+        raise ValueError("Chromosomes must have the same length")
 
     length = len(gene_a)
     if length < 2:
         return gene_a, gene_b
 
-    partition = randint(1, length-1)
-    return gene_a[0:partition] + gene_b[partition:], \
-        gene_b[0:partition] + gene_a[partition:]
+    partition = randint(1, length - 1)
+    return gene_a[0:partition] + gene_b[partition:], gene_b[0:partition] + gene_a[partition:]
 
 
-def mutation(genome: Genome, num: int = 1, probability: float = 0.5) -> Genome:
+def mutation(chromosome: Chromosome, num: int = 1, probability: float = 0.5) -> Chromosome:
     for _ in range(num):
-        # gets a random position based on genome length
-        index = randrange(len(genome))
-        genome[index] = genome[index] if random(
-        ) > probability else abs(genome[index] - 1)
-    return genome
+        # gets a random position based on chromosome length
+        index = randrange(len(chromosome))
+        chromosome[index] = chromosome[index] if random(
+        ) > probability else abs(chromosome[index] - 1)
+    return chromosome
 
 
-# convert the genome (list of integers) to a decimal number
-def convert_binary(genome: Genome) -> int:
+# convert the chromosome (list of integers) to a decimal number
+def convert_binary(chromosome: Chromosome) -> int:
     # [n, n, n] -> 'nnn'
-    parse_genome = ''.join(map(str, genome))
+    parse_chromosome = ''.join(map(str, chromosome))
     bin_prefix = '0b'
-    decimal = bin_prefix + parse_genome
+    decimal = bin_prefix + parse_chromosome
     return int(decimal, 2)
 
 
-# tests
-# print(crossover([1, 0, 0, 1, 1], [0, 0, 1, 0, 0]))  # crossover test
-# print(convert_binary([1, 0, 0]))  # binary to decimal test (0b111 = 4)
-# print(generate_population(10, 2))  # population generation (size = 2) test
-print(fitness([1, 1, 1], 7))
-print(fitness([1, 1, 1], 6))
-print(fitness([1, 1, 1], 5))
+def evolution_proccess(
+        population_size: int,
+        chromosome_length: int,
+        expected_result: int,
+        fitness_limit: int = 100,
+        generation_limit: int = 100) -> Tuple[Population, int]:
+    population = generate_population(population_size, chromosome_length)
+
+    for i in range(generation_limit):
+        population = sorted(
+            population,
+            key=lambda chromosome: fitness(chromosome, expected_result),
+            reverse=True
+        )
+        # prevents numbers out of the expected range
+        if fitness(population[0], expected_result) >= fitness_limit:
+            break
+
+        # gets the best pair of the current population
+        new_population = population[0:2]
+
+        for p in range(int(len(population) / 2) - 1):
+            parents = natural_selection(population)
+
+            offspring_a, offspring_b = crossover(parents[0], parents[1])
+            offspring_a = mutation(offspring_a)
+            offspring_b = mutation(offspring_b)
+
+            new_population += [offspring_a, offspring_b]
+
+        population = new_population
+
+    return population, i

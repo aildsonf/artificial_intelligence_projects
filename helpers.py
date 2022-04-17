3122da5b00ad03f7dc7cf258233a99841521b3d3
Aildson Ferreira <aildson.ferreira@icloud.com>
2022-04-17 20:08:24 -0300

    initial commit

---
+++ helpers.py
@@ -0,0 +1,92 @@
+import time
+
+
+flight_data = open('flights.txt')
+
+friends = [('Aildson', 'POA'), ('Thiago', 'GYN'), ('George', 'CNF'),
+           ('Julian', 'FLN'), ('Péricles', 'GIG'), ('Maigan', 'CWB')]
+domain = [(0, 9)] * (len(friends) * 2)
+
+destination = 'REC'
+flights = {}
+
+for flight in flight_data:
+    origin, destination, departure, arrival, price = flight.split(',')
+    flights.setdefault((origin, destination), [])
+    flights[(origin, destination)].append((departure, arrival, int(price)))
+
+
+def build_schedule(schedule):  # build a schedule based on a given solution
+    flight_id = -1
+
+    for i in range(len(schedule) // 2):
+        name = friends[i][0]
+        origin = friends[i][1]
+
+        flight_id += 1
+        trip = flights[(origin, destination)][schedule[flight_id]]
+
+        flight_id += 1
+        homecoming = flights[(destination, origin)][schedule[flight_id]]
+
+        print(
+            '%10s%6s -> %s %5s-%5s, $%3s %6s -> %s %5s-%5s, $%3s' %
+            (name, origin, destination, trip[0],
+             trip[1],
+             trip[2],
+             destination,
+             origin,
+             homecoming[0],
+             homecoming[1],
+             homecoming[2]))
+
+
+def get_minutes(hour):  # converts a time string to time in minutes (int)
+    parsed_hour = time.strptime(hour, '%H:%M')
+    minutes = parsed_hour[3] * 60 + parsed_hour[4]
+    return minutes  # ('2:00') -> 120
+
+
+def cost_function(solution):  # fitness function
+    cost = 0
+    wait_time = 0  # wait time of the first arrival to the last one
+    last_arrival = 0  # last person to arrive at destination
+    first_departure = get_minutes('23:59')  # first person to go back home
+    flight_id = -1
+
+    for i in range(len(solution) // 2):
+        origin = friends[i][1]
+
+        flight_id += 1
+        trip = flights[(origin, destination)][solution[flight_id]]
+
+        flight_id += 1
+        homecoming = flights[(destination, origin)][solution[flight_id]]
+
+        cost += trip[2]
+        cost += homecoming[2]
+
+        if last_arrival < get_minutes(trip[1]):
+            last_arrival = get_minutes(trip[1])
+
+        if first_departure > get_minutes(homecoming[0]):
+            first_departure = get_minutes(homecoming[0])
+
+    flight_id = -1
+
+    for i in range(len(solution) // 2):
+        origin = friends[i][1]
+
+        flight_id += 1
+        trip = flights[(origin, destination)][solution[flight_id]]
+
+        flight_id += 1
+        homecoming = flights[(destination, origin)][solution[flight_id]]
+
+        wait_time += last_arrival - get_minutes(trip[1])
+        wait_time += get_minutes(homecoming[0]) - first_departure
+
+    if last_arrival > first_departure:
+        cost += 50  # penalty for delays
+
+    return cost + wait_time  # fitness score
---
+++ main.py
@@ -0,0 +1,88 @@
+import random
+from helpers import domain, build_schedule, cost_function
+
+# Universidade Federal Rural de Pernambuco
+# DC - Departamento de Computacao
+# Inteligencia Artificial - 2021.1
+# Aildson Ferreira, Thiago Ferreira
+
+# Otimizacao de Parametros de um Classificador
+
+# O problema gira em torno de 6 amigos que pretendem
+# viajar ao mesmo destino; elas irao dividir o custo
+# total da viagem (total das passagens/ passagem adi-
+# cional por atraso)
+
+
+def mutation(domain, reference, solution):
+    i = random.randint(0, len(domain) - 1)  # position to change the value
+    mutant = solution
+
+    if random.random() < 0.5:  # probability to suffer mutation
+        if solution[i] != domain[i][0]:
+            mutant = solution[0:i] + [solution[i] - reference] + solution[i + 1:]
+        else:
+            if solution[i] != domain[i][1]:
+                mutant = solution[0:i] + [solution[i] + reference] + solution[i + 1:]
+
+    return mutant
+
+
+def crossover(domain, parent_1, parent_2):
+    i = random.randint(1, len(domain) - 2)  # position to trim
+    offspring = parent_1[0:i] + parent_2[i:]
+
+    return offspring
+
+
+def evolution(domain, population_size=1000, reference=1,
+              mutation_rate=0.1, elitism=0.2, generations=10000):
+    population = []
+
+    # build first population
+    for i in range(population_size):
+        solution = [random.randint(domain[i][0], domain[i][1])
+                    for i in range(len(domain))]
+        population.append(solution)
+
+    # calculates how many individuals to be chosen for the next gen
+    elitism_size = int(elitism * population_size)
+
+    # evolutionary proccess
+    for i in range(generations):
+        solution_cost = [(cost_function(individual), individual)
+                         for individual in population]
+        solution_cost.sort()
+
+        fittest_individuals = [individual for (
+            _, individual) in solution_cost]
+
+        population = fittest_individuals[0:elitism_size]
+
+        while len(population) < population_size:
+            # here we assume a possibility to either happen a mutation or
+            # a crossover
+            if random.random() < mutation_rate:
+                m = random.randint(0, elitism_size)
+                mutant = mutation(domain, reference, fittest_individuals[m])
+                population.append(mutant)
+            else:
+                chosen_parents = random.choices(
+                    # natural selection
+                    fittest_individuals,
+                    weights=[cost for (cost, _) in solution_cost],
+                    k=2
+                )
+                offspring = crossover(
+                    domain, chosen_parents[0],
+                    chosen_parents[1])
+                population.append(offspring)
+
+    return solution_cost[0][1]
+
+
+best_solution = evolution(domain)
+best_solution_cost = cost_function(best_solution)
+
+build_schedule(best_solution)
+print(f'\nTotal ${best_solution_cost}')
